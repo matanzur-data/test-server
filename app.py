08026371e1ef86123c90752e87e33b161cefcbfb
matanzur-data <matan.zur@yotpo.com>
2026-07-01 17:06:10 +0100

    Update app.py
    df

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def catch_all(path):
     print("=== SHOPIFY WEBHOOK ===")
     print(json.dumps(data, indent=2))
     print("======================")
-    return jsonify({"predicted_ltv": 100}), 200
+    return jsonify({"predicted_ltv": 200 , "value" : json.dumps(data, indent=2) }), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
